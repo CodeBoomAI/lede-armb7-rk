ea94611cb73d81168f5ee1e94dbb2f1e3aa7d5b6
pitekkate <kangristiktok@gmail.com>
2025-06-20 23:33:08 +0700

    Create armv7.mk

---
+++ target/linux/rockchip/image/armv7.mk
@@ -0,0 +1,10 @@
+define Device/rockchip_rk3229_hk1mini
+  DEVICE_VENDOR := Rockchip
+  DEVICE_MODEL := HK1 Mini
+  SOC := rk3229
+  DEVICE_DTS := rk3229-hk1mini
+  IMAGE_SIZE := 32m
+endef
+TARGET_DEVICES += rockchip_rk3229_hk1mini
+
+# Tambahkan device RK3229 lain jika perlu
