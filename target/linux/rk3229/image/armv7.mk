41504131de339206aa0403f61918553beace39bb
pitekkate <kangristiktok@gmail.com>
2025-06-20 08:53:49 +0700

    Create armv7.mk

---
+++ target/linux/rk3229/image/armv7.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2023 OpenWrt.org
+#
+
+define Image/BuildKernel
+    $(call Image/Build/CpImage,$(KDIR)/zImage, kernel)
+    $(call Image/Build/CpImage,$(KDIR)/$(DEVICE_DTS).dtb, dtb)
+endef
+
+define Image/BuildDeviceTree
+    $(call Image/Build/CpDTB,$(DEVICE_DTS))
+endef
+
+define Image/Build/squashfs
+    $(call Build/append-metadata)
+    $(call Image/Build/squashfs-ube)
+    $(call Image/Build/join-rootfs)
+endef
+
+define Image/Build
+    $(call Image/Build/$(1),$(device_name))
+endef
+
+# Tambahkan support untuk board tertentu
+$(eval $(call BuildImage))
